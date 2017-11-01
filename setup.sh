55571e8502fa166a3d316b64002f7c41026fa152
Stephen McLaughlin <stephen.mclaughlin@utexas.edu>
2017-10-31 20:46:53 -0500

    small edits

--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-
 cd /kaldi/egs/american-archive-kaldi/sample_experiment
 rm path.sh
 wget https://raw.githubusercontent.com/hipstas/kaldi-pop-up-archive/master/scripts/path.sh
 rm set-kaldi-path.sh
 wget https://raw.githubusercontent.com/hipstas/kaldi-pop-up-archive/master/scripts/set-kaldi-path.sh
+cd /kaldi/egs/wsj/s5/utils/
+rm run.pl
+wget https://raw.githubusercontent.com/hipstas/kaldi-pop-up-archive/master/scripts/run.pl
 
 mkdir /audio_in
+
 cd /audio_in
 
 ## Add audio to /audio_in
@@ -18,8 +21,8 @@ ffmpeg -i """$file""" -ac 1 -ar 16000 """$base"""_16kHz.wav;
 done
 
 mkdir /audio_in_16khz/
-
 mv *_16kHz.wav /audio_in_16khz/
 
+
 nohup python /kaldi/egs/american-archive-kaldi/run_kaldi.py /kaldi/egs/american-archive-kaldi/sample_experiment/ /audio_in_16khz/ && \
-mv /kaldi/egs/american-archive-kaldi/sample_experiment/output/ /
+rsync -a /kaldi/egs/american-archive-kaldi/sample_experiment/output/ /output/
